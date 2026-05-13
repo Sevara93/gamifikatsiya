32244d135f5ee6370686b4dbf2c9c6069e3ffe4e
mac <mac@macs-MacBook-Pro.local>
2026-05-13 23:51:14 +0500

    chore: clean up vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,12 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {allowedHosts: [".up.railway.app"]},
   server: {
     port: 3000,
     proxy: {
       '/api': 'http://localhost:3001',
-      
     },
   },
-  preview: {allowedHosts: [".up.railway.app"]}
 })
